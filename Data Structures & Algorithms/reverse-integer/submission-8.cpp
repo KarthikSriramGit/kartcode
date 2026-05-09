9196eaa82d695eaab0dea1a1f346e8c3cda15fbd
Karthik Sriram <51001690+KarthikSriramGit@users.noreply.github.com>
2026-05-09 16:09:52 -0700

    Add: reverse-integer - submission-8

---
+++ Data Structures & Algorithms/reverse-integer/submission-8.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while(x){
+            int curr = x % 10; x /= 10;
+            if (res > INT_MAX / 10 || (res == INT_MAX / 10 && curr > 7)) return 0;
+            if (res < INT_MIN / 10 || (res == INT_MIN / 10 && curr < -8)) return 0;
+            res = res * 10 + curr;
+        }
+        return res;
+    }
+};
