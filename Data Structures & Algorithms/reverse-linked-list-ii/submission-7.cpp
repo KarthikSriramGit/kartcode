1f24ca2b165319a72ce5b7d9ae555bbf102f0588
Karthik Sriram <51001690+KarthikSriramGit@users.noreply.github.com>
2026-05-25 22:58:37 -0700

    Add: reverse-linked-list-ii - submission-7

---
+++ Data Structures & Algorithms/reverse-linked-list-ii/submission-7.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* leftPrev = &dummy;
+        for(int i = 0; i < left - 1; i++){
+            leftPrev = leftPrev->next;
+        }
+        ListNode* leftNode = leftPrev->next;
+        ListNode* rightNode = leftNode;
+        for(int i = 0; i < right - left; i++){
+            rightNode = rightNode->next;
+        }
+        ListNode* rightNext = rightNode->next;
+        rightNode->next = nullptr;
+        leftPrev->next = nullptr;
+        ListNode* reversed = reverse(leftNode);
+        leftPrev->next = reversed;
+        leftNode->next = rightNext;
+        return dummy.next;
+    }
+    ListNode* reverse(ListNode* head){
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        while(curr){
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+};
